e6d7574a31add6c65a25e2660106e2001e59bb66
swimmingkiim <swimmingkiim@gmail.com>
2022-12-17 20:42:51 +0900

    Update with share link feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,30 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	export const prerender = true;
 
 	$: title = '';
+	$: showCopiedAlert = false;
+	$: showEmptyAlert = false;
+
+	const onCopyShareLink = async () => {
+		if (title.length <= 0) {
+			showEmptyAlert = true;
+			setTimeout(() => {
+				showEmptyAlert = false;
+			}, 3000);
+			return;
+		}
+		const host = import.meta.env.DEV ? 'http://localhost:5173' : 'https://swimmingkiim.github.io';
+		const url = `${host}${base}/${title}`;
+		await navigator.clipboard.writeText(url);
+		showCopiedAlert = true;
+		setTimeout(() => {
+			showCopiedAlert = false;
+		}, 3000);
+	};
 </script>
 
+<button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" on:click={onCopyShareLink}>Share</button>
 <div id="title-container">
 	<textarea
 		placeholder="Type here"
@@ -11,6 +32,44 @@
 		bind:value={title}
 	/>
 </div>
+{#if showCopiedAlert}
+	<div class="alert alert-success shadow-lg">
+		<div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="stroke-current flex-shrink-0 h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<span>Link copied!</span>
+		</div>
+	</div>
+{/if}
+{#if showEmptyAlert}
+	<div class="alert alert-error shadow-lg">
+		<div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="stroke-current flex-shrink-0 h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<span>Please enter your message</span>
+		</div>
+	</div>
+{/if}
 
 <style lang="scss">
 	#title-container {
@@ -41,5 +100,19 @@
 				background-color: #ffffff;
 			}
 		}
+	}
+	button {
+		margin: 1em 1.5em;
+
+		position: absolute;
+		z-index: 101;
+	}
+
+	.alert {
+		@apply w-[100%] md:w-[30%] lg:w-[60%];
+		position: fixed;
+		z-index: 102;
+		bottom: 1em;
+		@apply right-[0] md:right-[2.5%] lg:right-[5%];
 	}
 </style>
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,13 +1,41 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { base } from '$app/paths';
 	import type { PageData } from './$types';
 
 	/** @type {import('./$types').PageData} */
 	export let data: PageData;
 	export const prerender = true;
 
-	$: title = data.message ?? '';
+	$: title = '';
+	$: showCopiedAlert = false;
+	$: showEmptyAlert = false;
+
+	const onCopyShareLink = async () => {
+		if (title.length <= 0) {
+			showEmptyAlert = true;
+			setTimeout(() => {
+				showEmptyAlert = false;
+			}, 3000);
+			return;
+		}
+		const host = import.meta.env.DEV ? 'http://localhost:5173' : 'https://swimmingkiim.github.io';
+		const url = `${host}${base}/${title}`;
+		await navigator.clipboard.writeText(url);
+		showCopiedAlert = true;
+		setTimeout(() => {
+			showCopiedAlert = false;
+		}, 3000);
+	};
+
+	onMount(() => {
+		if (data.message) {
+			title = data.message;
+		}
+	});
 </script>
 
+<button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" on:click={onCopyShareLink}>Share</button>
 <div id="title-container">
 	<textarea
 		placeholder="Type here"
@@ -15,6 +43,44 @@
 		bind:value={title}
 	/>
 </div>
+{#if showCopiedAlert}
+	<div class="alert alert-success shadow-lg">
+		<div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="stroke-current flex-shrink-0 h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<span>Link copied!</span>
+		</div>
+	</div>
+{/if}
+{#if showEmptyAlert}
+	<div class="alert alert-error shadow-lg">
+		<div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="stroke-current flex-shrink-0 h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			>
+			<span>Please enter your message</span>
+		</div>
+	</div>
+{/if}
 
 <style lang="scss">
 	#title-container {
@@ -45,5 +111,19 @@
 				background-color: #ffffff;
 			}
 		}
+	}
+	button {
+		margin: 1em 1.5em;
+
+		position: absolute;
+		z-index: 101;
+	}
+
+	.alert {
+		@apply w-[100%] md:w-[30%] lg:w-[60%];
+		position: fixed;
+		z-index: 102;
+		bottom: 1em;
+		@apply right-[0] md:right-[2.5%] lg:right-[5%];
 	}
 </style>
