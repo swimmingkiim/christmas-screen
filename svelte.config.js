98b4bb115bd9d0d062a4de1b5b0bfb9c4d9cf06a
swimmingkiim <swimmingkiim@gmail.com>
2022-12-17 14:15:55 +0900

    Setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import postcss from 'postcss';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: vitePreprocess({postcss: true,}),
+	css: css => {
+        css.write('public/bundle.css')
+      },
 	kit: {
 		adapter: adapter()
 	}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	resolve: {
+        alias: {
+            src: path.resolve('src/'),
+        },
+    },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
