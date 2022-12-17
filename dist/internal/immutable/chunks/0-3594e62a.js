714e09ab7adbfbbc178ab8414e2e1745aaa7fedb
swimmingkiim <swimmingkiim@gmail.com>
2022-12-17 18:07:03 +0900

    Update build files

--- dist/internal/immutable/chunks/0-3594e62a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-26a7135f.js";export{t as component};
--- dist/internal/immutable/chunks/2-7fc2cd20.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-599ed90b.js";export{t as component};
