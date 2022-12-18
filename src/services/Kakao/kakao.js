1a7457d43bd5fc5cebfc26d8c891f206a57a3d04
swimmingkiim <swimmingkiim@gmail.com>
2022-12-18 23:11:22 +0900

    Update with kakao talk share image size

--- src/services/Kakao/kakao.js
+++ src/services/Kakao/kakao.js
@@ -18,8 +18,8 @@ export const shareWithKakao = (uri, message) => {
 		  description: message,
 		  imageUrl:
 			'https://github.com/swimmingkiim/blog-comments/blob/main/assets/images/%E1%84%92%E1%85%AA%E1%84%86%E1%85%A7%E1%86%AB_%E1%84%80%E1%85%B5%E1%84%85%E1%85%A9%E1%86%A8_2022-12-17_%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE_6_39_18_AdobeExpress.gif?raw=true',
-		  imageWidth: 1200,
-		  imageHeight: 630,
+		  imageWidth: 1920,
+		  imageHeight: 1080,
 		  link: {
 			mobileWebUrl: uri,
 		  },
